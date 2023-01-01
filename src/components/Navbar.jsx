48b68a03eed6f8e322811e702c868516de599405
Rhamsez Thevenin <rhamzthev@Rhamsezs-MacBook-Air.local>
2023-01-01 16:25:45 -0500

    created navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
-export default function Navbar() {}
+import styles from "./Navbar.module.scss";
+
+export default function Navbar() {
+    return (
+        <div className={styles.Navbar}>
+            <div className={styles.Navbar__Items}>
+                <ul className={styles.Items}>
+                    <li className={styles.Item}>About Me</li>
+                    <li className={styles.Item}>Experience</li>
+                    <li className={styles.MainItem}>Rhamsez Thevenin</li>
+                    <li className={styles.Item}>Projects</li>
+                    <li className={styles.Item}>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
