ead948351f2fc3f5bbfd4d6ba1a9008f0835b25d
RhamzThev <rhamzthev@gmail.com>
2023-01-01 14:02:59 -0500

    removed db for now

--- src/pages/db/index.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import Test from "./test";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default new Test(supabase);
